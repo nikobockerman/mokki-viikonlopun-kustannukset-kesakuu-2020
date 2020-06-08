3ba07250a2bffc27360153fce3d4969798ad4025
Niko Böckerman <niko.bockerman@iki.fi>
2020-06-08 08:38:38 +0300

    Add calculations

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# mokki-viikonlopun-kustannukset-kesakuu-2020
+# Mökkiviikonlopun kustannusten tasaus
+
+**Tapion polttoainekustannukset puuttuvat vielä**
+
+## Tämän hetkinen jako:
+```
+	Costs	Payments	To pay	To receive
+Ville	35.5	40		0	4.5
+Sami	35.5	0		35.5	0
+Niko	25	77.0		0	52.0
+Tomi	35.5	0		35.5	0
+Timo	35.5	75		0	39.5
+Tapio	25	0		25	0
+```
---
+++ calculate.py
@@ -0,0 +1,55 @@
+# Input data, first key is intended payer, second key is original payer.
+# Payments with intended payer "kaikki" get shared amongs all participants.
+payments = {
+    "kaikki": {
+        "Timo": 75,
+        "Niko": 35,
+        "Ville": 40,
+        "Tapio": 0,  # FIXME: Polttoainekustannukset
+    },
+    "Timo": {"Niko": 10.5},
+    "Sami": {"Niko": 10.5},
+    "Tomi": {"Niko": 10.5},
+    "Ville": {"Niko": 10.5},
+}
+participants = ["Timo", "Niko", "Ville", "Sami", "Tapio", "Tomi"]
+
+common_costs = sum(payments["kaikki"].values())
+
+
+def get_costs(name):
+    return common_costs / len(participants) + sum(payments.get(name, {}).values())
+
+
+def get_payments(name):
+    return sum([x.get(name, 0) for x in payments.values()])
+
+
+def print_results(calculations):
+    print("\tCosts\tPayments\tTo pay\tTo receive")
+    for name, c in calculations.items():
+        print(
+            "{}\t{}\t{}\t\t{}\t{}".format(
+                name, c["costs"], c["payments"], c["to_pay"], c["to_receive"]
+            )
+        )
+
+
+def main():
+    calculations = {}
+    for name in participants:
+        costs = get_costs(name)
+        payments = get_payments(name)
+        to_pay = max(0, costs - payments)
+        to_receive = max(0, payments - costs)
+        calculations[name] = {
+            "costs": costs,
+            "payments": payments,
+            "to_pay": to_pay,
+            "to_receive": to_receive,
+        }
+    print_results(calculations)
+
+
+if __name__ == "__main__":
+    main()
