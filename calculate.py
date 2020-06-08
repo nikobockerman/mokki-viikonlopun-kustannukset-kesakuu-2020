7e01e656b6d4949f3196d09865777631a1c7904d
Niko Böckerman <niko.bockerman@iki.fi>
2020-06-08 13:09:22 +0300

    Change input and internal data model

--- calculate.py
+++ calculate.py
@@ -1,50 +1,62 @@
-# Input data, first key is intended payer, second key is original payer.
-# Payments with intended payer "kaikki" get shared amongs all participants.
-payments = {
-    "kaikki": {"Timo": 75, "Niko": 35, "Ville": 40, "Tapio": 35},
-    "Timo": {"Niko": 10.5},
-    "Sami": {"Niko": 10.5},
-    "Tomi": {"Niko": 10.5},
-    "Ville": {"Niko": 10.5},
-    "Tapio": {"Ville": 8.5, "Sami": 27},
+input_data = {
+    "participants": ["Timo", "Niko", "Ville", "Sami", "Tapio", "Tomi"],
+    # payments: First key is original payer, second key is intended payer.
+    # Payments with intended payer "yhteinen" get shared amongst all participants.
+    "payments": {
+        "Timo": {"yhteinen": 75},
+        "Ville": {"yhteinen": 40, "Tapio": 8.5},
+        "Niko": {
+            "yhteinen": 35,
+            "Timo": 10.5,
+            "Sami": 10.5,
+            "Tomi": 10.5,
+            "Ville": 10.5,
+        },
+        "Tapio": {"yhteinen": 35},
+        "Sami": {"Tapio": 27},
+    },
 }
-participants = ["Timo", "Niko", "Ville", "Sami", "Tapio", "Tomi"]
 
-common_costs = sum(payments["kaikki"].values())
-
-
-def get_costs(name):
-    return common_costs / len(participants) + sum(payments.get(name, {}).values())
+payments = input_data["payments"]
+shared_costs = sum([value.get("yhteinen", 0) for value in payments.values()])
 
 
-def get_payments(name):
-    return sum([x.get(name, 0) for x in payments.values()])
+def get_participant_costs(name):
+    return shared_costs / len(input_data["participants"]) + sum(
+        [value.get(name, 0) for value in payments.values()]
+    )
 
 
-def print_results(calculations):
-    print("\tCosts\tPayments\tTo pay\tTo receive")
-    for name, c in calculations.items():
+def get_participant_payments(name):
+    return sum(payments.get(name, {}).values())
+
+
+class Participant:
+    def __init__(self, name, costs, payments):
+        self.name = name
+        self.costs = costs
+        self.payments = payments
+        self.to_pay = max(0, self.costs - self.payments)
+        self.to_receive = max(0, self.payments - self.costs)
+
+
+def print_results(participants):
+    print("Name\tCosts\tPayments\tTo pay\tTo receive")
+    for p in participants:
         print(
             "{}\t{}\t{}\t\t{}\t{}".format(
-                name, c["costs"], c["payments"], c["to_pay"], c["to_receive"]
+                p.name, p.costs, p.payments, p.to_pay, p.to_receive
             )
         )
 
 
 def main():
-    calculations = {}
-    for name in participants:
-        costs = get_costs(name)
-        payments = get_payments(name)
-        to_pay = max(0, costs - payments)
-        to_receive = max(0, payments - costs)
-        calculations[name] = {
-            "costs": costs,
-            "payments": payments,
-            "to_pay": to_pay,
-            "to_receive": to_receive,
-        }
-    print_results(calculations)
+    participants = []
+    for name in input_data["participants"]:
+        costs = get_participant_costs(name)
+        payments = get_participant_payments(name)
+        participants.append(Participant(name, costs, payments))
+    print_results(participants)
 
 
 if __name__ == "__main__":
