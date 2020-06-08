f17a687021c5abfaa4939822d04cfc8648be2d1c
Niko Böckerman <niko.bockerman@iki.fi>
2020-06-08 14:15:27 +0300

    Calculate transactions
    Optimized for minimal number of transactions.
    
    Updated readme with python3 output which calculates floats correctly.

--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 
 ## Tämän hetkinen tilanne:
 ```
-	Costs	Payments	To pay	To receive
-Ville	40.5	48.5		0	8.0
-Sami	40.5	27		13.5	0
-Niko	30	77.0		0	47.0
-Tomi	40.5	0		40.5	0
-Timo	40.5	75		0	34.5
-Tapio	65.5	35		30.5	0
+Name	Costs	Payments	To pay	To receive
+Timo	41.33	75.00		0.00	33.67
+Niko	30.83	77.00		0.00	46.17
+Ville	41.33	48.50		0.00	7.17
+Sami	41.33	27.00		14.33	0.00
+Tapio	66.33	35.00		31.33	0.00
+Tomi	41.33	0.00		41.33	0.00
+
+Transactions:
+Sami => Timo: 14.33
+Tapio => Timo: 19.34
+Tapio => Niko: 11.99
+Tomi => Niko: 34.18
+Tomi => Ville: 7.15
 ```
--- calculate.py
+++ calculate.py
@@ -17,14 +17,21 @@ input_data = {
     },
 }
 
+import decimal
+from decimal import Decimal as D
+
+decimal.getcontext().prec = 2
+
 payments = input_data["payments"]
 shared_costs = sum([value.get("yhteinen", 0) for value in payments.values()])
+# print("Shared costs: {}".format(shared_costs))
 
 
 def get_participant_costs(name):
-    return shared_costs / len(input_data["participants"]) + sum(
-        [value.get(name, 0) for value in payments.values()]
-    )
+    shared = shared_costs / len(input_data["participants"]) * 100 // 1 / 100
+    pays = [value.get(name, 0) for value in payments.values()]
+    # print("{}: shared={}; payments={} => {}".format(name, shared, pays, sum(pays)))
+    return shared + sum(pays)
 
 
 def get_participant_payments(name):
@@ -39,15 +46,77 @@ class Participant:
         self.to_pay = max(0, self.costs - self.payments)
         self.to_receive = max(0, self.payments - self.costs)
 
+    def to_pay_remaining(self, transactions):
+        return self.to_pay - sum([t.amount for t in transactions if t.p_from is self])
 
-def print_results(participants):
+    def to_receive_remaining(self, transactions):
+        return self.to_receive - sum([t.amount for t in transactions if t.p_to is self])
+
+
+class Transaction:
+    def __init__(self, p_from, p_to, amount):
+        self.p_from = p_from
+        self.p_to = p_to
+        self.amount = amount
+
+
+def get_possible_transactions(participants, transactions):
+    possible_transactions = []
+    for p_from in participants:
+        to_pay_remaining = p_from.to_pay_remaining(transactions)
+        # print("To pay remaining: {} = {}".format(p_from.name, to_pay_remaining))
+        if to_pay_remaining <= 0:
+            continue
+
+        for p_to in participants:
+            if p_to is p_from:
+                continue
+
+            to_receive_remaining = p_to.to_receive_remaining(transactions)
+            # print("To receive remaining: {} = {}".format(p_to.name, to_receive_remaining))
+            if to_receive_remaining <= 0:
+                continue
+
+            possible_transactions.append(
+                Transaction(p_from, p_to, min(to_pay_remaining, to_receive_remaining))
+            )
+
+    return possible_transactions
+
+
+def get_transactions(participants, already_made_transactions):
+    possible_transactions = get_possible_transactions(
+        participants, already_made_transactions
+    )
+    if not possible_transactions:
+        yield already_made_transactions
+        return
+
+    for possible_transaction in possible_transactions:
+        transactions = already_made_transactions[:]
+        transactions.append(possible_transaction)
+        for combo in get_transactions(participants, transactions):
+            yield combo
+
+
+def get_best_transactions(participants):
+    transaction_combos = list(get_transactions(participants, []))
+    transaction_combos.sort(key=lambda combo: len(combo))
+    return transaction_combos[0]
+
+
+def print_results(participants, transactions):
     print("Name\tCosts\tPayments\tTo pay\tTo receive")
     for p in participants:
         print(
-            "{}\t{}\t{}\t\t{}\t{}".format(
-                p.name, p.costs, p.payments, p.to_pay, p.to_receive
+            "{}\t{:.2f}\t{:.2f}\t\t{:.2f}\t{:.2f}".format(
+                p.name, D(p.costs), D(p.payments), D(p.to_pay), D(p.to_receive)
             )
         )
+    print()
+    print("Transactions:")
+    for t in transactions:
+        print("{} => {}: {:.2f}".format(t.p_from.name, t.p_to.name, t.amount))
 
 
 def main():
@@ -56,7 +125,8 @@ def main():
         costs = get_participant_costs(name)
         payments = get_participant_payments(name)
         participants.append(Participant(name, costs, payments))
-    print_results(participants)
+    transactions = get_best_transactions(participants)
+    print_results(participants, transactions)
 
 
 if __name__ == "__main__":
